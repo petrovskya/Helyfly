0d1fefc9c68c6eb7d1b5d7d617f2a5302f8f1c55
Yana Piatrouskaya <Yana.Piatrouskaya@itechart-group.com>
2023-03-16 15:03:24 +0300

    fix: change naming, apply prettier, use arrow functions, replace forEach with map

--- index.js
+++ index.js
@@ -1,49 +1,67 @@
-const $SLIDER = document.querySelector('.location__wrapper');
+let FIRST_SLIDE_INDEX = 0;
+let LAST_SLIDE_INDEX = 2;
+let CURRENT_SLIDE_INDEX = FIRST_SLIDE_INDEX;
 
-const prevSlideBtn = $SLIDER.querySelector('.slide-prev');
-const nextSlideBtn = $SLIDER.querySelector('.slide-next');
-const slides = $SLIDER.querySelectorAll('.slider__item');
+const LOCATION_WRAPPER_CLASSNAME = '.location__wrapper';
+const PREVIOUS_SLIDE_CLASSNAME = '.slide-previous';
+const NEXT_SLIDE_CLASSNAME = '.slide-next';
+const SLIDER_ITEM_CLASSNAME = '.slider__item';
+const SLIDER_DOT_CLASSNAME = '.slider__dot';
+const ACTIVE_ITEM_CLASSNAME = 'active';
+const CHECKED_ITEM_CLASSNAME = 'checked';
 
-const sliderDots = document.querySelectorAll('.slider__dot');
-let currentSlideIndex = 0;
+const $LOCATION_WRAPPER = document.querySelector(LOCATION_WRAPPER_CLASSNAME);
+const PREVIOUS_SLIDE_BUTTON = $LOCATION_WRAPPER.querySelector(
+  PREVIOUS_SLIDE_CLASSNAME
+);
+const NEXT_SLIDE_BUTTON = $LOCATION_WRAPPER.querySelector(NEXT_SLIDE_CLASSNAME);
+const SLIDES = $LOCATION_WRAPPER.querySelectorAll(SLIDER_ITEM_CLASSNAME);
+const SLIDER_DOTS = Array.from(
+  $LOCATION_WRAPPER.querySelectorAll(SLIDER_DOT_CLASSNAME)
+);
 
-const showPrevSlide = function () {
-  hideSlide(currentSlideIndex);
-  if (currentSlideIndex == 0) {
-    currentSlideIndex = slides.length - 1;
-    showSlide(currentSlideIndex);
+const hideSlide = (index) => {
+  SLIDES[index].classList.remove(ACTIVE_ITEM_CLASSNAME);
+  SLIDER_DOTS[index].classList.remove(CHECKED_ITEM_CLASSNAME);
+};
+
+const showSlide = (index) => {
+  SLIDES[index].classList.add(ACTIVE_ITEM_CLASSNAME);
+  SLIDER_DOTS[index].classList.add(CHECKED_ITEM_CLASSNAME);
+};
+
+const showPreviousSlide = () => {
+  hideSlide(CURRENT_SLIDE_INDEX);
+  if (CURRENT_SLIDE_INDEX == FIRST_SLIDE_INDEX) {
+    CURRENT_SLIDE_INDEX = LAST_SLIDE_INDEX;
+    showSlide(CURRENT_SLIDE_INDEX);
   } else {
-    currentSlideIndex--;
-    showSlide(currentSlideIndex);
+    CURRENT_SLIDE_INDEX--;
+    showSlide(CURRENT_SLIDE_INDEX);
   }
 };
-const nextPrevSlide = function () {
-  hideSlide(currentSlideIndex);
-  if (currentSlideIndex >= slides.length - 1) {
-    currentSlideIndex = 0;
-    showSlide(currentSlideIndex);
+
+const showNextSlide = () => {
+  hideSlide(CURRENT_SLIDE_INDEX);
+  if (CURRENT_SLIDE_INDEX >= LAST_SLIDE_INDEX) {
+    CURRENT_SLIDE_INDEX = FIRST_SLIDE_INDEX;
+    showSlide(CURRENT_SLIDE_INDEX);
   } else {
-    currentSlideIndex++;
-    showSlide(currentSlideIndex);
+    CURRENT_SLIDE_INDEX++;
+    showSlide(CURRENT_SLIDE_INDEX);
   }
 };
 
-const hideSlide = function (index) {
-  slides[index].classList.remove('active');
-  sliderDots[index].classList.remove('checked');
-};
-const showSlide = function (index) {
-  slides[index].classList.add('active');
-  sliderDots[index].classList.add('checked');
+const showCheckedSlide = (index) => {
+  hideSlide(CURRENT_SLIDE_INDEX);
+  showSlide(index);
+  CURRENT_SLIDE_INDEX = index;
 };
 
-prevSlideBtn.addEventListener('click', showPrevSlide);
-nextSlideBtn.addEventListener('click', nextPrevSlide);
+PREVIOUS_SLIDE_BUTTON.addEventListener('click', showPreviousSlide);
 
-sliderDots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    hideSlide(currentSlideIndex);
-    showSlide(index);
-    currentSlideIndex = index;
-  });
+NEXT_SLIDE_BUTTON.addEventListener('click', showNextSlide);
+
+SLIDER_DOTS.map((dot, index) => {
+  dot.addEventListener('click', () => showCheckedSlide(index));
 });
